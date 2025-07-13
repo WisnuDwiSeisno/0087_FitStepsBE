284c1bd674b1e28113bdcc45a1befbad2568b5ae
WisnuDwiSeisno <wseisno37@gmail.com>
2025-07-13 21:54:14 +0700

    model untuk user

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,48 +1,60 @@
 <?php
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    // JWT Methods
+    public function getJWTIdentifier()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    // Relasi dengan tabel lain
+    public function challengesCreated()
+    {
+        return $this->hasMany(Challenge::class, 'created_by');
+    }
+
+    public function participations()
+    {
+        return $this->hasMany(ChallengeParticipation::class);
+    }
+
+    public function steps()
+    {
+        return $this->hasMany(StepsLog::class);
+    }
+
+    public function routes()
+    {
+        return $this->hasMany(RouteLog::class);
+    }
+
+    public function tips()
+    {
+        return $this->hasMany(Tip::class, 'mentor_id');
     }
 }
